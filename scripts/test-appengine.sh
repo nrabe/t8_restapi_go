e8d31a937ddf10e29f4b68d31cf8a14a2e2dbdcf
nrabe <nrabe2@gmail.com>
2014-05-20 15:33:18 -0300

    re-enabled error testing

--- scripts/test-appengine.sh
+++ scripts/test-appengine.sh
@@ -6,7 +6,7 @@ GOAPP=~/google-cloud-sdk/platform/google_appengine/goapp
 
 ENDPOINT=http://t8-restapi-go.appspot.com/restaurant/0.1/
 
-#env python tests_remote/test_errors.py "$ENDPOINT"
+env python tests_remote/test_errors.py "$ENDPOINT"
 RV=$?; if [ $RV -ne 0 ]; then exit $RV; fi
 
 env python tests_remote/test_general.py "$ENDPOINT"
